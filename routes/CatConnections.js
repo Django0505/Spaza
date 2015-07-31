24a8cc33d561b4411a53b58b319b26bfb1844149
Mawande06 <mawande@projectcodex.co>
2015-07-31 15:19:58 +0200

    created CatConnections to decouple web page logic from database for categories

---
+++ routes/CatConnections.js
@@ -0,0 +1,46 @@
+module.exports = function(connection){
+
+var outputQuery = function(query,cb){
+    connection.query(query, cb);
+
+};
+
+var inputQuery = function(query, data, cb){
+connection.query(query, data, cb);
+
+};
+this.showCategories = function(cb){
+    outputQuery('SELECT * FROM categories', cb);
+
+};
+
+this.insertCategory = function(data, cb){
+    inputQuery('INSERT INTO categories SET ?',data, cb);
+
+};
+
+this. deletingCategory = function(data, cb){
+    inputQuery('DELETE FROM categories WHERE category_id = ?', data, cb);
+};
+
+this.updatingCategory = function(data, cb){
+    inputQuery('UPDATE categories SET ? WHERE category_id = ?', data, cb);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,23 @@
+var mysql = require('mysql');
+var CatConnections = require('./CatConnections');
+var connection =  mysql.createConnection({
+ host: 'localhost',
+        user: 'root',
+        password: 'spot',
+        port: 3306,
+        database: 'spaza'
+});
+
+connection.connect();
+//connection.query('use spaza');
+var catConnections = new CatConnections(connection);
+
+
+
 // Show categories
 exports.showCatList = function(req, res, next) {
 
-    req.getConnection(function(err, connection) {
-        if (err)
-            return next(err);
-        connection.query('SELECT * from categories', [], function(err, results, fields) {
+  catConnections.showCategories(function(err, results, fields) {
             if (err) return next(err);
             var Admin = false;
             if (req.session.role == "Admin")
@@ -16,41 +29,36 @@ exports.showCatList = function(req, res, next) {
 
             });
         });
-    });
+
 };
 
 //==Adding category== 
 exports.addCat = function(req, res, next) {
-    req.getConnection(function(err, connection) {
-        if (err) {
-            return next(err);
-        }
+
 
         var input = JSON.parse(JSON.stringify(req.body));
         var data = {
             category_name: input.category_name
         };
-        connection.query('insert into categories set ?', data, function(err, results) {
+       catConnections.insertCategory(data,function(err, results) {
             if (err)
                 return console.log("Error inserting : %s ", err);
 
-            res.redirect('/CatList')
+            res.redirect('/CatList');
         });
-    });
 
-}
+};
 
 //==deleting a category==
 exports.deleteCat = function(req, res, next) {
     var id = req.params.id;
-    req.getConnection(function(err, connection) {
-        connection.query('DELETE FROM categories WHERE category_id = ?', [id], function(err, rows) {
+    catConnections.deletingCategory([id],function(err, rows) {
             if (err) {
                 console.log("Error Selecting : %s ", err);
             }
             res.redirect('/CatList');
         });
-    });
+
 };
 
 
@@ -59,15 +67,13 @@ exports.updateCat = function(req, res, next) {
 
     var data = JSON.parse(JSON.stringify(req.body));
     var id = req.params.id;
-    req.getConnection(function(err, connection) {
-        connection.query('UPDATE categories SET ? WHERE category_id = ?', [data, id], function(err, rows) {
+    catConnections.updatingCategory([data,id],function(err, rows) {
             if (err) {
                 console.log("Error Updating : %s ", err);
             }
             res.redirect('/CatList');
         });
 
-    });
 };
 
 
